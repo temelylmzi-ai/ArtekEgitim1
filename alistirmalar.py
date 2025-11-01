940f3690063320db6ec99a433650b1467745c4b8
Talha Yılmaz <temelylmzi@gmail.com>
2025-11-01 23:08:58 +0300

    Create alistirmalar.py

---
+++ alistirmalar.py
@@ -0,0 +1,14 @@
+musteriAdi = "Talha"
+musteriSoyadi = "Yilmaz"
+musteriAdiSoyadi = musteriAdi + " " + musteriSoyadi
+musteriYasi = 22    
+musteriDogumYili = 2003
+musteriCinsiyeti = "Erkek"
+musteriAdresi = "Kocaeli, Turkiye"
+
+siparis1 = 2499.99
+siparis2 = 1500.50
+siparis3 = 799.75
+toplamSiparisTutari = siparis1 + siparis2 + siparis3
+print(toplamSiparisTutari)
+
