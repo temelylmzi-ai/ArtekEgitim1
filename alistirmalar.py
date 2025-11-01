ee9693717394700d616f6e362f46d69c7a3126cf
Talha Yılmaz <temelylmzi@gmail.com>
2025-11-01 23:11:24 +0300

    Update alistirmalar.py

--- alistirmalar.py
+++ alistirmalar.py
@@ -5,6 +5,11 @@ musteriYasi = 22
 musteriDogumYili = 2003
 musteriCinsiyeti = "Erkek"
 musteriAdresi = "Kocaeli, Turkiye"
+print("Musteri Bilgileri:")
+print("Ad Soyad:", musteriAdiSoyadi)
+print("Yas:", musteriYasi)
+musteriDogumYili, musteriCinsiyeti, musteriAdresi = 2003, "Erkek", "Kocaeli, Turkiye"
+
 
 siparis1 = 2499.99
 siparis2 = 1500.50
