95ed4b1a846e19d9dd817180b2c61099f31acf4e
Talha Yılmaz <temelylmzi@gmail.com>
2025-11-01 23:16:11 +0300

    Update alistirmalar.py

--- alistirmalar.py
+++ alistirmalar.py
@@ -5,11 +5,10 @@ musteriYasi = 22
 musteriDogumYili = 2003
 musteriCinsiyeti = "Erkek"
 musteriAdresi = "Kocaeli, Turkiye"
-print("Musteri Bilgileri:")
+print("Musteri Bilgileri\n")
 print("Ad Soyad:", musteriAdiSoyadi)
 print("Yas:", musteriYasi)
-musteriDogumYili, musteriCinsiyeti, musteriAdresi = 2003, "Erkek", "Kocaeli, Turkiye"
-
+print(f"{musteriDogumYili}, {musteriCinsiyeti}, {musteriAdresi}")
 
 siparis1 = 2499.99
 siparis2 = 1500.50
